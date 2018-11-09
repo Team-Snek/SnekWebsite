0c50e06a8697a55fefa0b88c9c2220d104d819a5
Nate Turner <nathanturner595@gmail.com>
2018-11-09 14:56:30 -0700

    Added snakes view

--- SnekWebsite/Controllers/HomeController.cs
+++ SnekWebsite/Controllers/HomeController.cs
@@ -4,6 +4,7 @@
  11/8/2018
  */
 
+using SnekWebsite.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,8 +15,26 @@ namespace SnekWebsite.Controllers
 {
     public class HomeController : Controller
     {
+        //Create list of snake objects.
+        public List<Snake> liSnakes = new List<Snake>();
+
+        public void AddSnakes()
+        {
+            liSnakes.Add(new Snake(1, "Artemis", "Nate", "Corn snake", "4 years", "This girl is sassy, but super sweet!", "URL"));
+            liSnakes.Add(new Snake(2, "Athena", "Kelly", "Corn snake", "4 years", "This girl is soooo snuggly!", "URL"));
+            liSnakes.Add(new Snake(3, "Midas", "Nate & Kelly", "Corn snake", "1 year", "Tiny, cute butter noodle.", "URL"));
+        }
+
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult Snakes()
+        {
+            AddSnakes();
+            ViewBag.SnakeList = liSnakes;
+
             return View();
         }
 
--- SnekWebsite/Models/Snake.cs
+++ SnekWebsite/Models/Snake.cs
@@ -16,5 +16,21 @@ namespace SnekWebsite.Models
         public string snakeAge { get; set; }
         public string snakeTalk { get; set; }
         public string linkToPic { get; set; }
+
+        public Snake()
+        {
+
+        }
+
+        public Snake(int id, string snakename, string ownername, string breed, string age, string comments, string link)
+        {
+            this.snakeID = id;
+            this.snakeName = snakename;
+            this.ownerName = ownername;
+            this.snakeBreed = breed;
+            this.snakeAge = age;
+            this.snakeTalk = comments;
+            this.linkToPic = link;
+        }
     }
 }
