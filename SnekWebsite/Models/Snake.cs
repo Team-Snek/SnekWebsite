5907bf2a1bb5fd4971a1463d294554a631e18285
Nate Turner <nathanturner595@gmail.com>
2018-11-09 14:25:38 -0700

    Adding snake talk property (where people can say whatever they want about their snakes)

--- SnekWebsite/Models/Snake.cs
+++ SnekWebsite/Models/Snake.cs
@@ -14,6 +14,7 @@ namespace SnekWebsite.Models
         public string ownerName { get; set; }
         public string snakeBreed { get; set; }
         public string snakeAge { get; set; }
+        public string snakeTalk { get; set; }
         public string linkToPic { get; set; }
     }
 }
